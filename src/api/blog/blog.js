3bf036ecef2d70f68b6c78af49a2e4440d822637
小恸恸 <wtt150249@gmail.com>
2023-07-28 13:30:27 +0800

    feat: 为请求结果添加缓存

--- src/api/blog/blog.js
+++ src/api/blog/blog.js
@@ -1,22 +1,52 @@
 import http from "../axios.js";
 
+const catchs = new Map();
+
+// 目前不需要自动清除缓存，因为后端数据一般情况下不会改变，在数据改变时，手动清除缓存即可
+// // 检查 catchs 内的数据是否过期
+// setInterval(() => {
+// 	for (const [key, value] of catchs) {
+// 		if (value.expires < Date.now()) {
+// 			catchs.delete(key);
+// 		}
+// 	}
+// }, 1000 * 60 * 5); // 每5分钟检查一次
+
 async function addActicle(body) {
+	// 数据添加后，清除缓存
+	catchs.clear();
+
 	return await http.post("/acticle/add", body);
 }
 
 async function getActicleList(filters) {
-	return await http.get(
+	const filterCategory = filters?.category || "";
+
+	// 如果缓存中有数据，直接返回缓存中的数据
+	if (catchs.has("acticle" + filterCategory)) {
+		return catchs.get("acticle" + filterCategory).value;
+	}
+
+	const data = await http.get(
 		"/acticle/list",
-		filters?.category
+		filterCategory
 			? {
 					params: {
 						filters: {
-							category: filters.category
+							category: filterCategory
 						}
 					}
 			  }
 			: undefined
 	);
+
+	// 将数据缓存
+	catchs.set("acticle" + filterCategory, {
+		value: data,
+		expires: Date.now() + 1000 * 60 * 10 // 设置缓存时间为10分钟，其实没有意义，因为数据不会改变
+	});
+
+	return data;
 }
 
 async function getActicleById(id) {
@@ -24,10 +54,16 @@ async function getActicleById(id) {
 }
 
 async function editActicleById(id, body) {
+	// 数据修改后，清除缓存
+	catchs.clear();
+
 	return await http.put(`/acticle/edit/${id}`, body);
 }
 
 async function deleteActicleById(id) {
+	// 数据删除后，清除缓存
+	catchs.clear();
+
 	return await http.delete(`/acticle/delete/${id}`);
 }
 
--- src/api/blog/word.js
+++ src/api/blog/word.js
@@ -1,11 +1,37 @@
 import http from "../axios.js";
 
+const catchs = new Map();
+
+// 目前不需要自动清除缓存，因为后端数据一般情况下不会改变，在数据改变时，手动清除缓存即可
+// // 检查 catchs 内的数据是否过期
+// setInterval(() => {
+// 	for (const [key, value] of catchs) {
+// 		if (value.expires < Date.now()) {
+// 			catchs.delete(key);
+// 		}
+// 	}
+// }, 1000 * 60 * 5); // 每5分钟检查一次
+
 async function addWord(body) {
+	// 数据添加后，清除缓存
+	catchs.clear();
+
 	return await http.post("/word/add", body);
 }
 
 async function getWordList() {
-	return await http.get("/word/list");
+	if (catchs.has("word")) {
+		return catchs.get("word").value;
+	}
+
+	const data = await http.get("/word/list");
+
+	catchs.set("word", {
+		value: data,
+		expires: Date.now() + 1000 * 60 * 10 // 设置缓存时间为10分钟，其实没有意义，因为数据不会改变
+	});
+
+	return data;
 }
 
 export { addWord, getWordList };
