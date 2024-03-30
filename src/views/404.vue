5a7c8ba116d980324b7a1226d2d4467dc16399e1
小恸恸 <wtt150249@gmail.com>
2024-03-30 21:34:53 +0800

    feat: 为 404 页面适配日文界面

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,13 +3,26 @@
 		<h1 hidden class="nf-404">404</h1>
 		<img class="img" :src="photo" alt="404 Not Found" />
 
-		<p>涟没有找到想要的内容，呜呜，涟真没用，对不起</p>
-		<p>不过有个前辈告诉我 “迷子でもいい、迷子でも進め!”</p>
-		<p>需要涟带你去<nami-link to="/" class="return-home">首页</nami-link>吗？</p>
+		<template v-if="i18nStore.lang === 'ja'">
+			<p>探していたものが見つかりませんでした！ぴえん,役に立てない、ごめん(˃ ⌑ ˂ഃ )</p>
+			<p>でも、先輩が教えてくれた「迷子でもいい、迷子でも進め!」という言葉を思い出します。</p>
+			<p>
+				ナミと一緒に<nami-link to="/" class="return-home">ホーム</nami-link>へ行きませんか？
+			</p>
+		</template>
+		<template v-else>
+			<p>涟没有找到想要的内容，呜呜，涟真没用，对不起</p>
+			<p>不过有个前辈告诉我 “迷子でもいい、迷子でも進め!”</p>
+			<p>需要涟带你去<nami-link to="/" class="return-home">首页</nami-link>吗？</p>
+		</template>
 	</section>
 </template>
 
 <script setup lang="ts">
+import { useI18nStore } from "@/stores/i18n";
+
+const i18nStore = useI18nStore();
+
 const photo = "https://image.xtt.moe/images/404.webp";
 </script>
 
