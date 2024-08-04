ef958e60d8e097fb455c5884d8e97d8681872cb9
小恸恸 <wtt150249@gmail.com>
2024-08-04 11:19:06 +0800

    feat: 更新阅读清单

--- src/views/reading/index.vue
+++ src/views/reading/index.vue
@@ -1,18 +1,13 @@
 <template>
 	<section class="container web-color-default">
-		<xtt-markdown :dark="store.isDark ? '' : undefined">{{
-			i18nStore.lang === "ja" ? jaReadingContent : readingContent
-		}}</xtt-markdown>
+		<xtt-markdown :dark="store.isDark ? '' : undefined">{{ readingContent }}</xtt-markdown>
 	</section>
 </template>
 
 <script setup lang="ts">
 import readingContent from "./reading.md";
-import jaReadingContent from "./reading.ja.md";
 import { useStore } from "@/stores/index";
-import { useI18nStore } from "@/stores/i18n";
 
-const i18nStore = useI18nStore();
 const store = useStore();
 </script>
 
