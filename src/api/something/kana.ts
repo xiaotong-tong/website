2725dbdcd89374d418ada8ce675f091650012b9f
小恸恸 <wtt150249@gmail.com>
2024-10-13 18:05:09 +0800

    feat: pinyin转为后端请求

--- src/api/something/kana.ts
+++ src/api/something/kana.ts
@@ -1,7 +1,15 @@
 import http from "../axios";
 
 export async function toKana(message: string): Promise<string> {
-	const data = await http.post("/kana/tokana", {
+	const data = await http.post("/ruby/kana/tokana", {
+		message: message
+	});
+
+	return data.data;
+}
+
+export async function toPinyin(message: string): Promise<string> {
+	const data = await http.post("/ruby/pinyin/toPinyin", {
 		message: message
 	});
 
--- src/views/somethings/pinyin.vue
+++ src/views/somethings/pinyin.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="container">
+	<section class="container web-color-default">
 		<hBanner wrapperTargetName="h1" class="text-center">
 			<span v-if="i18nStore.lang === 'ja'">中国語の漢字の読み方</span>
 			<ruby v-else class="-translate-y-1"> 拼音<rt>pīnyīn</rt> </ruby>
@@ -12,19 +12,40 @@
 			:placeholder="i18nStore.lang === 'ja' ? '中国語を入力してください' : '请输入中文'"
 		/>
 
-		<NamiButton @click="transform" :borderColor="store.currentTheme">
-			{{ i18nStore.lang === "ja" ? "OK" : "确定" }}
-		</NamiButton>
-		<NamiButton
-			class="ms-4"
-			:borderColor="store.currentTheme"
-			@click="copy"
-			v-if="isSupported && isParsed"
-		>
-			<span v-if="!copied">{{ i18nStore.lang === "ja" ? "コピー" : "复制" }}</span>
-			<span v-else>{{ i18nStore.lang === "ja" ? "コピーしました" : "复制成功" }}</span>
-		</NamiButton>
+		<div class="flex mt-2">
+			<NamiButton @click="transform" :borderColor="store.currentTheme" :loading="tranLoading">
+				{{ i18nStore.lang === "ja" ? "変更する" : "转换" }}</NamiButton
+			>
+			<NamiButton class="ms-4" :borderColor="store.currentTheme" @click="copy" v-if="isSupported && isParsed">
+				<span v-if="!copied">{{ i18nStore.lang === "ja" ? "コピー" : "复制" }}</span>
+				<span v-else>{{ i18nStore.lang === "ja" ? "コピーしました" : "复制成功" }}</span>
+			</NamiButton>
+			<NamiButton
+				v-if="kanaHistory.size"
+				class="ms-auto"
+				:borderColor="store.currentTheme"
+				@click="kanaHistory.clear()"
+				>清除缓存</NamiButton
+			>
+		</div>
 		<div class="ruby-text" v-html="rubyText"></div>
+
+		<div class="history" v-if="kanaHistory.size">
+			<hBanner wrapperTargetName="h3" class="text-center">{{
+				i18nStore.lang === "ja" ? "履歴" : "历史"
+			}}</hBanner>
+			<div class="history-content">
+				<div v-for="[key, value] in kanaHistory" :key="key" class="flex items-center mt-1 gap-x-2">
+					<div class="w-[140px] flex-none">
+						{{ formatDate(key, "yyyy-MM-DD HH:mm:ss") }}
+					</div>
+					<NButton aria-label="复制" text @click="copy($event, value)"
+						><namiIcon icon="mdiContentCopy"></namiIcon
+					></NButton>
+					<div v-html="value"></div>
+				</div>
+			</div>
+		</div>
 	</section>
 
 	<Teleport to="head">
@@ -33,34 +54,47 @@
 </template>
 
 <script setup lang="ts">
-import { html } from "pinyin-pro";
 import { ref } from "vue";
-import { NInput } from "naive-ui";
+import { NInput, NButton } from "naive-ui";
+import { toPinyin } from "@/api/something/kana";
 import { useI18nStore } from "@/stores/i18n";
+import confetti from "canvas-confetti";
+import { useClipboard, useLocalStorage } from "@vueuse/core";
 import { useStore } from "@/stores";
 import { hBanner, NamiButton } from "@c/index";
-import confetti from "canvas-confetti";
-import { useClipboard } from "@vueuse/core";
-
-const store = useStore();
-const i18nStore = useI18nStore();
-const rubyText = ref("");
-const isParsed = ref(false);
-const value = ref("");
+import { formatDate } from "xtt-utils";
 
 const { copy: useCopy, isSupported, copied } = useClipboard();
 
-const transform = () => {
+const store = useStore();
+const i18nStore = useI18nStore();
+
+const rubyText = ref("");
+const value = ref("");
+const isParsed = ref(false);
+const tranLoading = ref(false);
+
+const kanaHistory = useLocalStorage("pinyin_history", new Map<number, string>());
+
+const transform = async () => {
 	if (!value.value) {
 		return;
 	}
-	rubyText.value = html(value.value);
+	tranLoading.value = true;
+	rubyText.value = await toPinyin(value.value);
 	isParsed.value = true;
+	tranLoading.value = false;
+
+	kanaHistory.value.set(Date.now(), rubyText.value);
 };
-const copy = async () => {
-	// 将 rubyText 的内容复制到剪贴板
+
+const copy = async (_: any, value?: string) => {
+	if (!value) {
+		value = rubyText.value;
+	}
+
 	try {
-		await useCopy(rubyText.value);
+		await useCopy(value);
 
 		confetti({
 			particleCount: 100,
@@ -78,5 +112,10 @@ const copy = async () => {
 	margin-block-start: 16px;
 	line-height: 2.5;
 	letter-spacing: 2px;
+}
+.history-content {
+	display: flex;
+	flex-direction: column-reverse;
+	row-gap: 8px;
 }
 </style>
