a7853814899e1d242cbed879b69738cd967a20a6
小恸恸 <wtt150249@gmail.com>
2024-10-14 23:59:28 +0800

    feat: 一些md页面使用本地markdown组件

--- src/views/reading/index.vue
+++ src/views/reading/index.vue
@@ -1,14 +1,10 @@
 <template>
-	<section class="container">
-		<xtt-markdown :dark="store.isDark ? '' : undefined">{{ readingContent }}</xtt-markdown>
-	</section>
+	<markdown :content="readingContent"></markdown>
 </template>
 
 <script setup lang="ts">
 import readingContent from "./reading.md";
-import { useStore } from "@/stores/index";
-
-const store = useStore();
+import { markdown } from "@c/index";
 </script>
 
 <style scoped></style>
--- src/views/tools/tools.vue
+++ src/views/tools/tools.vue
@@ -1,19 +1,15 @@
 <template>
 	<hBanner wrapperTargetName="h1">Tools</hBanner>
-	<xtt-markdown :dark="store.isDark ? '' : undefined">{{
-		i18nStore.lang === "ja" ? jaLinkContent : linkContent
-	}}</xtt-markdown>
+	<markdown :content="i18nStore.lang === 'ja' ? jaLinkContent : linkContent"></markdown>
 </template>
 
 <script setup lang="ts">
 import linkContent from "./tools.md";
 import jaLinkContent from "./tools.ja.md";
-import { useStore } from "@/stores/index";
 import { useI18nStore } from "@/stores/i18n";
-import { hBanner } from "@c/index";
+import { hBanner, markdown } from "@c/index";
 
 const i18nStore = useI18nStore();
-const store = useStore();
 </script>
 
 <style scoped></style>
