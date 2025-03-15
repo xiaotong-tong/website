aafa2c5ffb3c7e556f7f72c83f884ee8c984bdfb
小恸恸 <wtt150249@gmail.com>
2025-03-15 21:51:12 +0800

    feat: 添加异次元之旅

--- src/components/page/footer/footer.vue
+++ src/components/page/footer/footer.vue
@@ -1,15 +1,27 @@
 <template>
 	<footer class="footer web-color-default">
 		<div class="content">
-			<div>
+			<div class="text-[#39c]">
 				CopyRight © 2023-2025 <nami-link class="link" to="/">星川漣の家</nami-link> |
 				<nami-link class="link" to="/sitemap">
 					{{ i18nStore.messages.main.footer.sitemap }}
 				</nami-link>
 			</div>
-			<div class="text-center">
-				<nami-link class="link" to="https://icp.gov.moe/?keyword=20249550" target="_blank">
+			<div class="text-center flex w-full items-center justify-center">
+				<nami-link class="text-[#39c]" to="https://icp.gov.moe/?keyword=20249550" target="_blank">
 					萌ICP备20249550号
+				</nami-link>
+				<span class="pl-1 pr-1 text-[#39c]">|</span>
+				<nami-link
+					class="text-[#e77c8e] flex items-center"
+					to="https://travel.moe/go.html"
+					target="_blank"
+					title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！"
+				>
+					<img
+						src="https://travel.moe/images/icon/icon64pink.png"
+						style="width: 24px; height: 24px"
+					/>异次元之旅
 				</nami-link>
 			</div>
 		</div>
@@ -33,9 +45,5 @@ const i18nStore = useI18nStore();
 .content {
 	font-size: 14px;
 	color: #999;
-}
-
-.link {
-	color: #39c;
 }
 </style>
